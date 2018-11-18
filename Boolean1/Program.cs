2bcd2cc04c3175c7dedec1d2cf8bc97ec7768d9e
BrusilovE <brusilov_e@mail.ru>
2018-11-18 23:17:47 +0500

    Добавьте файлы проекта.

---
+++ Boolean1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool s;
+            int a;
+            Console.Write("введите целое число = ");
+            a = Convert.ToInt32(Console.ReadLine());
+            s = a > 0;  
+            Console.Write("число является положительным = " + s );
+            Console.ReadLine();
+        }
+    }
+}
